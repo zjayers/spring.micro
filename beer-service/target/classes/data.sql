30667cda1f159e8ffbf6d4e2b600b98d52f08640
Zach Ayers <ayers.dev@icloud.com>
2020-12-19 20:05:36 -0600

    updated gitignore

--- beer-service/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO beers (id, beer_name, beer_style, created_date, updated_date, min_on_hand, quantity_to_brew, price, upc,
-                   version)
-VALUES ('0a818933-087d-47f2-ad83-2f986ed087eb', 'Mango Bobs', 'IPA', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, 200,
-        12.95, '0631234200036', 1),
-       ('a712d914-61ea-4623-8bd0-32c0f6545bfd', 'Galaxy Cat', 'PALE_ALE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL,
-        200,
-        12.95, '0631234300019', 1),
-       ('026cc3c8-3a0c-4083-a05b-e908048c1b08', 'Pinball Porter', 'PORTER', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL,
-        200, 12.95, '0083783375213', 1);
